20439006faf9992daa1c7d080db3508f7c74b0de
ramon_ramirez <marluxia122@hotmail.com>
2020-04-09 00:34:12 -0600

    redesing of mainpage and modal nav

--- scrollsections.js
+++ scrollsections.js
@@ -1,9 +1,25 @@
 
 
-
+let modalisshown=0;
 function scrolltodesired(target){
     document.getElementById(target).scrollIntoView({ 
         behavior: 'smooth' 
     });
+    document.getElementById("modal").classList.remove("modalshow");
+    modalisshown=0;
 
+}
+function openmodal(){
+
+    if(modalisshown==0){
+        document.getElementById("modal").classList.add("modalshow");
+        modalisshown=1;
+
+    }
+    else{
+        document.getElementById("modal").classList.remove("modalshow");
+        modalisshown=0;
+
+    }
+    
 }
